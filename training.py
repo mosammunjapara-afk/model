adb4550cb75e2636cc19436a08c7dd2a9842b31a
mosammunjapara-afk <mosammunjapara@gmail.com>
2026-02-03 14:22:05 +0530

    Update training.py

--- training.py
+++ training.py
@@ -66,7 +66,8 @@ history = model.fit(
     validation_data=test_data
 )
 
-model.save("fruit_model.keras")
+model.save("fruit_model.h5")
 
 print("Training Accuracy:", history.history["accuracy"][-1] * 100)
 print("Testing Accuracy:", history.history["val_accuracy"][-1] * 100)
+
